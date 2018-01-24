23355155b7d26d0cd404b36930a01637e24a3b77
Alexis Koss <alexis30@uw.edu>
2018-01-24 01:46:43 -0800

    added functions that add or subtract two given points

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -22,8 +22,16 @@ class Calculator {
         return total;
     }
     
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1);
+    }
+    
     func subtract(lhs: Int, rhs: Int) -> Int {
         return lhs - rhs;
+    }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1);
     }
     
     func multiply(lhs: Int, rhs: Int) -> Int {
